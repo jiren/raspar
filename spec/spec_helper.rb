099cb60710d58f033ac50a435a790f41c4b68d8c
Jiren Patel <jiren@joshsoftware.com>
2013-05-28 19:45:27 +0530

    Set rspec config for color and sepcdoc

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,17 @@ require 'rubygems'
 require 'bundler/setup'
 require 'open-uri'
 
+RSpec.configure do |config|
+  config.color_enabled = true
+  config.tty = true
+  config.formatter = :documentation
+end
+
 $:.unshift(File.dirname(__FILE__) + '/../lib/')
 
 require 'raspar'
 
-REAL_PAGE = open('spec/html/test.htm')
+#REAL_PAGE = open('spec/html/test.htm')
 
 class TestParser
 end
