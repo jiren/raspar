1a4865f3663af046fffa64ad7b7a0ecf2840da1b
Jiren Patel <jiren@joshsoftware.com>
2014-06-18 13:31:11 +0530

    Removed color schema for rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,7 @@ require 'coveralls'
 Coveralls.wear!
 
 RSpec.configure do |config|
-  config.color_enabled = true
+  #config.color_enabled = true
   #config.tty = true
   #config.formatter = :documentation
 end
